7bc6443496b7d568fdc590837403f7666b51a64c
Maddymahendra <navishfast@gmail.com>
2026-09-16 23:20:56 +0530

    Add SEO foundation for DeshFiles

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,21 @@
+import type { APIRoute } from "astro";
+
+const urls = [
+  "https://deshfiles.com/",
+  "https://deshfiles.com/tools/pdf-to-jpg/",
+  "https://deshfiles.com/tools/jpg-to-pdf/",
+  "https://deshfiles.com/tools/merge-pdf/"
+];
+
+export const GET: APIRoute = () => {
+  const body = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${urls.map((url) => `  <url><loc>${url}</loc></url>`).join("\n")}
+</urlset>`;
+
+  return new Response(body, {
+    headers: {
+      "Content-Type": "application/xml; charset=utf-8"
+    }
+  });
+};
